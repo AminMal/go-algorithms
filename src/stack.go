f9542e9945d997e1518062eb31bb1a8869e2fac0
AminMal <amin.malekloo@yahoo.com>
2020-11-18 18:14:10 +0330

    Stack structure in go

---
+++ src/stack.go
@@ -0,0 +1,39 @@
+package main
+
+import(
+    "fmt"
+    "errors"
+)
+
+type MyStack struct {
+    elements []int
+}
+
+func (s MyStack) push(element int) {
+    s.elements = append(s.elements, element)
+}
+
+func (s MyStack) pop() (int, MyStack, error) {
+    if len(s.elements) == 0 {
+	return -1, MyStack{elements: nil}, errors.New("Stack is empty")
+    } else {
+        value := s.elements[len(s.elements) - 1]
+        return value, MyStack{elements: s.elements[:len(s.elements) - 1]}, nil
+    }
+}
+
+func main() {
+
+    source := make([]int, 4)
+    for i := 0; i < 4; i++ { // 0, 2, 4, 6
+	source[i] = i * 2
+    }
+
+    var stack MyStack = MyStack{elements: source}
+    lastElem, stackAfterThat, _ := stack.pop()
+
+
+    fmt.Println("Stack: ", stack)
+    fmt.Println("Last element: ", lastElem)
+    fmt.Println("Stack after popping one element: ", stackAfterThat)
+}

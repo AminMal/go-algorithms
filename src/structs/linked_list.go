59c03b7b46664415f1c9dc608a6d2bb560f01490
AminMal <amin.malekloo@yahoo.com>
2020-11-22 21:31:12 +0330

    linked list, initiali

---
+++ src/structs/linked_list.go
@@ -0,0 +1,40 @@
+package main
+
+import(
+	"errors"
+)
+
+type Node struct {
+	data int
+	previous *Node
+	next *Node
+}
+
+type LinkedList struct {
+	nodes []Node
+}
+
+func (ll LinkedList) Append(element int) LinkedList {
+	if len(ll.nodes) == 0 {
+		return LinkedList{nodes: []Node{Node{data: element}}}
+	} else {
+		last := ll.nodes[len(ll.nodes) - 1]
+		node := Node{data: element, previous: &last}
+		updatedLeft := Node{data: last.data, previous: last.previous, next: &node}
+		return LinkedList{nodes: append(ll.nodes[:len(ll.nodes) - 1], updatedLeft, node)}
+	}
+}
+
+func (ll LinkedList) Prepend(element int) LinkedList {
+	if len(ll.nodes) == 0 {
+		return LinkedList{nodes: []Node{Node{data: element}}}
+	} else {
+		right := ll.nodes[0]
+		node := Node{data: element, next: &right}
+		updatedRight := Node {data: right.data, next: right.next, previous: &node}
+		slice := []Node{node, updatedRight}
+		return LinkedList{
+			nodes: append(slice, ll.nodes[1:]...),
+		}
+	}
+}
